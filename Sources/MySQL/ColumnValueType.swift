c5c2e70fed3d14c9161f4f99cf471de005ed46af
zunda <47569369+zunda-pixel@users.noreply.github.com>
2023-12-20 02:28:03 +0900

    move to ColumnValueType

--- Sources/MySQL/ColumnValueType.swift
+++ Sources/MySQL/ColumnValueType.swift
@@ -1,3 +1,5 @@
+import CMySQL
+
 public enum ColumnValueType {
   case decimal
   case tiny
@@ -28,4 +30,39 @@ public enum ColumnValueType {
   case varString
   case string
   case geometry
+  
+  public init(from type: enum_field_types) {
+    switch type {
+    case MYSQL_TYPE_DECIMAL: self = .decimal
+    case MYSQL_TYPE_TINY: self = .tiny
+    case MYSQL_TYPE_SHORT: self = .short
+    case MYSQL_TYPE_LONG: self = .long
+    case MYSQL_TYPE_FLOAT: self = .float
+    case MYSQL_TYPE_DOUBLE: self = .double
+    case MYSQL_TYPE_NULL: self = .null
+    case MYSQL_TYPE_TIMESTAMP: self = .timestamp
+    case MYSQL_TYPE_LONGLONG: self = .longLong
+    case MYSQL_TYPE_INT24: self = .int24
+    case MYSQL_TYPE_DATE: self = .date
+    case MYSQL_TYPE_TIME: self = .time
+    case MYSQL_TYPE_DATETIME: self = .dateTime
+    case MYSQL_TYPE_YEAR: self = .year
+    case MYSQL_TYPE_VARCHAR: self = .varchar
+    case MYSQL_TYPE_BIT: self = .bit
+    case MYSQL_TYPE_TIMESTAMP2: self = .timestamp2
+    case MYSQL_TYPE_INVALID: self = .invalid
+    case MYSQL_TYPE_JSON: self = .json
+    case MYSQL_TYPE_NEWDECIMAL: self = .newDecimal
+    case MYSQL_TYPE_ENUM: self = .enum
+    case MYSQL_TYPE_SET: self = .set
+    case MYSQL_TYPE_TINY_BLOB : self = .tinyBlob
+    case MYSQL_TYPE_MEDIUM_BLOB: self = .mediumBlob
+    case MYSQL_TYPE_LONG_BLOB: self = .longBlob
+    case MYSQL_TYPE_BLOB: self = .blob
+    case MYSQL_TYPE_VAR_STRING: self = .varString
+    case MYSQL_TYPE_STRING: self = .string
+    case MYSQL_TYPE_GEOMETRY: self = .geometry
+    default: fatalError()
+    }
+  }
 }
--- Sources/MySQL/MySQL.swift
+++ Sources/MySQL/MySQL.swift
@@ -7,8 +7,6 @@ public struct SQLDatabase: Database {
   
   public init(configration: Configuration) {
     self.configration = configration
-    //let mysql: UnsafeMutablePointer<MYSQL>! = nil
-    //mysql_options(mysql, MYSQL_OPT_CONNECT_TIMEOUT, nil)
     self.mysql = mysql_init(nil)
   }
   
@@ -23,6 +21,8 @@ public struct SQLDatabase: Database {
       configration.unixSocket,
       configration.clientFlag ? 0 : 1
     )
+    
+    try handleError()
     
     if result == nil {
       throw ConnectionError.failedConnect
@@ -42,67 +42,48 @@ public struct SQLDatabase: Database {
   
   public func close() async throws {
     mysql_close(self.mysql)
+    try handleError()
   }
   
   public func commit() async throws {
     mysql_commit(mysql)
+    try handleError()
   }
   
   public func beginTransaction() async throws {
     mysql_stat(mysql)
+    try handleError()
   }
   
   public func shutdown() async throws {
     mysql_shutdown(mysql, SHUTDOWN_DEFAULT)
+    try handleError()
   }
   
   func rollBack() async throws {
     mysql_rollback(mysql)
+    try handleError()
   }
   
-  public func valueType(from type: enum_field_types) -> ColumnValueType {
-    switch type {
-    case MYSQL_TYPE_DECIMAL: return .decimal
-    case MYSQL_TYPE_TINY: return .tiny
-    case MYSQL_TYPE_SHORT: return .short
-    case MYSQL_TYPE_LONG: return .long
-    case MYSQL_TYPE_FLOAT: return .float
-    case MYSQL_TYPE_DOUBLE: return .double
-    case MYSQL_TYPE_NULL: return .null
-    case MYSQL_TYPE_TIMESTAMP: return .timestamp
-    case MYSQL_TYPE_LONGLONG: return .longLong
-    case MYSQL_TYPE_INT24: return .int24
-    case MYSQL_TYPE_DATE: return .date
-    case MYSQL_TYPE_TIME: return .time
-    case MYSQL_TYPE_DATETIME: return .dateTime
-    case MYSQL_TYPE_YEAR: return .year
-    case MYSQL_TYPE_VARCHAR: return .varchar
-    case MYSQL_TYPE_BIT: return .bit
-    case MYSQL_TYPE_TIMESTAMP2: return .timestamp2
-    case MYSQL_TYPE_INVALID: return .invalid
-    case MYSQL_TYPE_JSON: return .json
-    case MYSQL_TYPE_NEWDECIMAL: return .newDecimal
-    case MYSQL_TYPE_ENUM: return .enum
-    case MYSQL_TYPE_SET: return .set
-    case MYSQL_TYPE_TINY_BLOB : return .tinyBlob
-    case MYSQL_TYPE_MEDIUM_BLOB: return .mediumBlob
-    case MYSQL_TYPE_LONG_BLOB: return .longBlob
-    case MYSQL_TYPE_BLOB: return .blob
-    case MYSQL_TYPE_VAR_STRING: return .varString
-    case MYSQL_TYPE_STRING: return .string
-    case MYSQL_TYPE_GEOMETRY: return .geometry
-    default: fatalError()
-    }
+  func handleError() throws {
+    guard let error = mysql_error(mysql) else { return }
+    print(String(validatingUTF8: error)!)
   }
   
   public func query(_ query: String) async throws -> [[String: (type: ColumnValueType, data: Data)]] {
-    guard mysql_real_query(mysql, query, UInt(query.utf8.count)) == 0 else {
+    let queryResult = mysql_real_query(mysql, query, UInt(query.utf8.count))
+    
+    try handleError()
+    
+    guard queryResult == 0 else {
       fatalError()
     }
-
+    
     guard let response: UnsafeMutablePointer<MYSQL_RES> = mysql_use_result(mysql) else {
       fatalError()
     }
+    
+    try handleError()
     
     defer {
       mysql_free_result(response)
@@ -110,20 +91,26 @@ public struct SQLDatabase: Database {
     
     let fieldCount = Int(mysql_num_fields(response))
     
+    try handleError()
+
     guard let fields = mysql_fetch_fields(response) else { fatalError() }
-        
+
+    try handleError()
+
     var allRows: [[String: (ColumnValueType, Data)]] = []
     
     while true {
       guard let row = mysql_fetch_row(response) else { break }
+      try handleError()
       guard let lengths = mysql_fetch_lengths(response) else { fatalError() }
+      try handleError()
       var columns: [String: (ColumnValueType, Data)] = [:]
       
       for i in 0..<fieldCount {
         guard let pointer = row[i] else { fatalError() }
         let field = fields[i]
         guard let key = String(validatingUTF8: field.name) else { fatalError() }
-        let type = valueType(from: field.type)
+        let type = ColumnValueType(from: field.type)
         let value = Data(bytes: UnsafeRawPointer(pointer), count: Int(lengths[i]))
         columns[key] = (type, value)
       }
